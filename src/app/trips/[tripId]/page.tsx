66cb852b14f1665fcde3e7fe8edcf5b29188b599
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-07-07 20:56:49 -0300

    feat: add trip reservation component

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/lib/prisma";
 import TripHeader from "./components/TripHeader";
+import TripReservation from "./components/TripReservation";
 
 async function getTripDetails(tripId: string) {
   const trip = await prisma.trip.findUnique({
@@ -23,6 +24,8 @@ export default async function TripDetails({
   return (
     <div className="container mx-auto">
       <TripHeader trip={trip} />
+
+      <TripReservation trip={trip} />
     </div>
   );
 }
