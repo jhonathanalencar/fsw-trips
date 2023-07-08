15414bdd267cc7133b10c10b7117f51d0fa8166b
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-07-07 22:03:38 -0300

    feat: add location description

---
+++ prisma/migrations/20230708004300_add_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
