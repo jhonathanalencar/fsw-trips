52c782c714f69ecd0bf5358a203261ee896573c3
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-07-07 21:13:16 -0300

    feat: add trip description component

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import TripHeader from "./components/TripHeader";
 import TripReservation from "./components/TripReservation";
+import TripDescription from "./components/TripDescription";
 
 async function getTripDetails(tripId: string) {
   const trip = await prisma.trip.findUnique({
@@ -26,6 +27,7 @@ export default async function TripDetails({
       <TripHeader trip={trip} />
 
       <TripReservation trip={trip} />
+      <TripDescription description={trip.description} />
     </div>
   );
 }
