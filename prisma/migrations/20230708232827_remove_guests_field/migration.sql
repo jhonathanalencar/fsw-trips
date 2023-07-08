010c15fed8d3f515154fa78212157243b6ff465c
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-07-08 20:32:25 -0300

    feat: add new migration

---
+++ prisma/migrations/20230708232827_remove_guests_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `guests` on the `TripReservation` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "TripReservation" DROP COLUMN "guests";
